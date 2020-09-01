81e67b21fbc26abb9c3ad12f64a0e4577da22aa8
OLIVIAMANDOLA <ovm5126@psu.edu>
2020-09-01 21:09:33 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+// Author: Olivia Mandola ovm5126@psu.edu
+// Collaborator: Tyler Holman trh5481@psu.edu
+// Collaborator: Michael Sullivan mls6888@psu.edu
+
+int main(void) {
+
+  double cel, fah;
+char* temp = readline("Enter temperature in celsius: " );
+cel = atof(temp);
+fah = ((cel * 1.8) + 32);
+printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", cel, fah);
+
+  return 0;
+}
